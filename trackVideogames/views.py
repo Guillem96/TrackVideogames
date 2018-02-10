af1c0452bc5fb2c86cf9597d1ddd1a4aff343902
Guillem96 <guillem.orellana@gmail.com>
2018-02-10 16:40:57 +0100

    Starting front-end

--- trackVideogames/views.py
+++ trackVideogames/views.py
@@ -2,5 +2,36 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+
+# Security Mixins
+class LoginRequiredMixin(object):
+    @method_decorator(login_required())
+    def dispatch(self, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
+
+class CheckIsOwnerMixin(object):
+    def get_object(self, *args, **kwargs):
+        obj = super(CheckIsOwnerMixin, self).get_object(*args, **kwargs)
+        if not obj.user == self.request.user:
+            raise PermissionDenied
+        return obj
+
+class LoginRequiredCheckIsOwnerUpdateView(LoginRequiredMixin, CheckIsOwnerMixin, UpdateView):
+    template_name = 'form.html'
+
 
 # Create your views here.
+def home_page(request):
+    return render(
+        request,
+        'index.html',
+    )
+
